7b0c76be1b47b7c2b95b8cc171208516467ccd8d
Stephen Okafor <stephen.okafor@cwg-plc.com>
2025-06-03 11:11:30 +0100

    Gluestack and Product screen

--- ecommerce-mobile/app/_layout.tsx
+++ ecommerce-mobile/app/_layout.tsx
@@ -1,6 +1,12 @@
 import {  Stack } from "expo-router";
-
+import '@/global.css'
+// import the glustack here
+import { GluestackUIProvider } from "@/components/ui/gluestack-ui-provider";
 
 export default function RootLayout() {
-    return <Stack />
+    return (
+        <GluestackUIProvider mode="dark">
+            <Stack/>
+        </GluestackUIProvider>
+    )
 }
--- ecommerce-mobile/components/ProductListItem.tsx
+++ ecommerce-mobile/components/ProductListItem.tsx
@@ -1,9 +1,36 @@
-import { Text } from 'react-native'
+// import { Text } from 'react-native'
+import { Card } from '@/components/ui/card'
+import { Image } from '@/components/ui/image'
+import { Heading } from '@/components/ui/heading'
+import { Text } from '@/components/ui/text'
+import { Link } from 'expo-router'
+import { Pressable } from 'react-native'
 
 
 // my flat list component
 export default function ProductListItem({ product }){
     return (
-        <Text style={{ fontSize : 16}}>{product.name}</Text>
+        <Link href={`/product/${product.id}`} asChild>
+            <Pressable className='flex-1'>
+                <Card className="p-5 rounded-lg max-w-[360px] flex-1">
+                        <Image
+                            source={{
+                                uri: product.image,
+                            }}
+                            className="mb-6 h-[240px] w-full rounded-md aspect-[4/3]"
+                            alt={`${product.name} image`}
+                            resizeMode='contain'
+                        />
+                        <Text className="text-sm font-normal mb-2 text-typography-700">
+                            {product.name}
+                        </Text>
+                            <Heading size="md" className="mb-3">
+                            {product.price}
+                            </Heading>
+                    </Card>
+                </Pressable>
+            </Link>
+        
+        // <Text style={{ fontSize : 16}}>{product.name}</Text>
     )
 }
